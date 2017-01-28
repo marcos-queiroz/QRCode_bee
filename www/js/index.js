2f6464dbdd68107ad692425d0b38a5daef715c12
Marcos Queiroz <marcosmqueiroz@live.com>
2017-01-28 18:43:04 -0200

    QR Code funcionando

--- www/js/index.js
+++ www/js/index.js
@@ -1,54 +1,13 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    },
+function onDeviceReady() {
+    console.log(navigator.camera);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+    lerQRCode();
+}
 
-        lerQRCode();
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
-
-function lerQRCode(){
+// Função para LER QRCode
+function lerQRCode() {
     console.log("Ler QRCode");
 
     cordova.plugins.barcodeScanner.scan(
@@ -63,11 +22,11 @@ function lerQRCode(){
             alert("Scanning failed: " + error);
         },
         {
-            preferFrontCamera : true, // iOS and Android
+            preferFrontCamera : false, // iOS and Android
             showFlipCameraButton : true, // iOS and Android
             showTorchButton : true, // iOS and Android
             torchOn: true, // Android, launch with the torch switched on (if available)
-            prompt : "Place a barcode inside the scan area", // Android
+            prompt : "Coloque um código de barras dentro da área de digitalização", // Android
             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
             formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
             orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
